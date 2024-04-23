d2a129da1cc92d06160755d633878f91c0a7aed9
Monir Hosain <me.monirhosain@gmail.com>
2024-04-23 16:01:31 +0600

    repo updated

---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+
+ Notes:
+ HTML এ দুই ধরণের ট্যাগ দিয়ে লেখা হয় ১) Opening Tag 
+                                   2) Closing Tag 
+                           
+ h1 -> h6 => Heading বুঝাতে ব্যাবহার করা হয়
+ p -> paragraph tag , paragraph তৈরীর জন্যে ব্যাবহার করা হয়
+ br -> line এর মাঝখানে ব্রেক দেওয়ার জন্যে ব্যাবহার করা হয়
+     *** br tag এর কোন ending tag নেই ***
+     
+
+  a -> link তৈরীতে a tag ব্যাবহার করা হয়
+  img -> image নিয়ে আসার জন্যে img tag ব্যাবহার করা হয়
+      *** img tag এর কোন ending tag নেই ***
+
+  i -> কোন লেখাকে বাকিয়ে লিখতে i tag ব্যাবহার করা হয়
+  sup -> কোন অক্ষর কে পাওয়ার হিসেবে লিখতে super script tag ব্যাবহার করা হয়
+  sub -> কোন অক্ষর বা সংখ্যা কে নিচে নিতে বা রাসায়নিক সংকেত লিখতে ব্যাবহার করা হয় 
+  b -> কোন টেক্সট কে বোল্ড করতে bold tag ব্যাবহার করা হয়
+  strong -> Important text বুঝাতে strong tag ব্যাবহার করা হয়
+  em -> Emphasized text লিখতে em tag ব্যাবহার করা হয়
+  mark -> কোন কিছু হাইলাইট করে লিখতে mark tag ব্যাবহার করা হয়
+  del -> কোন লেখার মাঝখান দিয়ে দাগ দিতে ট্যাগটি ব্যাবহার করা হয়
+  ins -> inserted text লিখতে ট্যাগটি ব্যাবহার করা হয়
+  
+
+  ol -> Orderd List তৈরি করতে ol ব্যাবহার করা হয়
+  ul -> Unorderd List তৈরি করতে ul ব্যাবহার করা হয়
+  li -> List Item তৈরি করতে li ব্যাবহার করা হয় 
+  
+  dl -> description list 
+  dt -> description title
+  dd -> description detail
+
+  blockquote -> কোন কিছু কোট করার জন্য ব্যাবহার করা হয়
+  q -> কোটেশন ব্যাবহার করার জন্য q tag ব্যাবহার করা হয়
+  abbr -> সংক্ষিপ্ত রূপ এর মাধ্যমে পূর্ণরূপ প্রকাশ করতে abbr ব্যাহার করা হয়
+  address -> ঠিকানা লিখতে ব্যাবহার করা হয়
+  bdo ->  bdo এর পূর্ণরূপ Bi-Directional ... লেখার দিক নির্ধারণ করার জন্য bdo ট্যাগ ব্যাবহার করা হয় 
+        bdo এর দুইটি attribute হল 
+                            i. rtl => right to left
+                            ii. ltr => left to right
+
+
+*** div -> block level container , কোন কিছু কে যদি ধারণ করে তাহলে সম্পূর্ণ ডিস্প্লেয় ব্লক করে রাখে 
+*** span -> inline container , কোন কিছু কে যদি ধারণ করে তাহলে সম্পূর্ণ ডিস্প্লেয় ব্লক করে রাখে না
+
+
+  
+*/
